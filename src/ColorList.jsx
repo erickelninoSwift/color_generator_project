1edbed5b3a04e9943c97086ea6cc616b586de0b6
Erick El nino <erick.tshimbombo@yahoo.com>
2023-08-20 02:26:30 +0200

    update data

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SingleColor } from "./SingleColor";
+import { v4 as uuidv4 } from "uuid";
 
 export const ColorList = ({ myColors }) => {
   return (
     <section className="colors">
-      {myColors.map((color) => {
-        return <SingleColor colors={color} />;
+      {myColors.map((color, index) => {
+        let id = uuidv4();
+        return <SingleColor key={id} colors={color} index={index} />;
       })}
     </section>
   );
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export const SingleColor = ({ colors }) => {
-  return <div>{colors}</div>;
+export const SingleColor = ({ colors, index }) => {
+  const { hex, weight } = colors;
+  const backgroundStyle = {
+    backgroundColor: `#${hex}`,
+  };
+
+  const indexBackground = () => {};
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ backgroundColor: `#${hex}` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
 };
